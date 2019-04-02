9b97cd7d9e1747bdff472900ec7d7171d6dceff1
ExZiByte <exzibyte@gmail.com>
2019-04-02 01:31:36 -0500

    Added Randomized game status changer

---
+++ src/main/java/me/exzibyte/modulus/OnReady.java
@@ -0,0 +1,27 @@
+package me.exzibyte.modulus;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class OnReady extends ListenerAdapter {
+
+    public void onReady(ReadyEvent event) {
+        Game[] games = { Game.playing("with modular bits!"), Game.watching("Project Modulus"), Game.watching("TV"), Game.streaming("Modularity", "https://twitch.tv/exzibyte") };
+        ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+        ses.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                Random random = new Random();
+                int game = random.nextInt(games.length);
+                System.out.println("Switching game to: " + games[game]);
+                event.getJDA().getPresence().setGame(games[game]);
+            }
+        }, 0, 2, TimeUnit.MINUTES);
+    }
+}
